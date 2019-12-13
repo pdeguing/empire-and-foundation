1feff0c912bd11f5659fa62340264be535fb455f
Pierre-Quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-12-13 17:29:40 +0100

    fixed input data in planet tabs

--- route_dashboard.go
+++ route_dashboard.go
@@ -62,29 +62,64 @@ func planet(w http.ResponseWriter, r *http.Request) {
 // GET /dashboard/planet/constructions
 // Show the constructions page for a planet
 func constructions(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, r, nil, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.constructions")
+	u := loggedInUser(r)
+	p, err := u.QueryPlanets().
+		First(r.Context())
+	if err != nil {
+		internalServerError(w, r, err, "Could not retrieve user's planet from database")
+		return
+	}
+	generateHTML(w, r, p, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.constructions")
 }
 
 // GET /dashboard/planet/factories
 // Show the factories page for a planet
 func factories(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, r, nil, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.factories")
+	u := loggedInUser(r)
+	p, err := u.QueryPlanets().
+		First(r.Context())
+	if err != nil {
+		internalServerError(w, r, err, "Could not retrieve user's planet from database")
+		return
+	}
+	generateHTML(w, r, p, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.factories")
 }
 
 // GET /dashboard/planet/research
 // Show the research page for a planet
 func research(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, r, nil, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.research")
+	u := loggedInUser(r)
+	p, err := u.QueryPlanets().
+		First(r.Context())
+	if err != nil {
+		internalServerError(w, r, err, "Could not retrieve user's planet from database")
+		return
+	}
+	generateHTML(w, r, p, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.research")
 }
 
 // GET /dashboard/planet/fleets
 // Show the fleets page for a planet
 func fleets(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, r, nil, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.fleets")
+	u := loggedInUser(r)
+	p, err := u.QueryPlanets().
+		First(r.Context())
+	if err != nil {
+		internalServerError(w, r, err, "Could not retrieve user's planet from database")
+		return
+	}
+	generateHTML(w, r, p, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.fleets")
 }
 
 // GET /dashboard/planet/defenses
 // Show the defenses page for a planet
 func defenses(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, r, nil, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.defenses")
+	u := loggedInUser(r)
+	p, err := u.QueryPlanets().
+		First(r.Context())
+	if err != nil {
+		internalServerError(w, r, err, "Could not retrieve user's planet from database")
+		return
+	}
+	generateHTML(w, r, p, "layout", "private.navbar", "dashboard", "rightbar", "planet.layout", "planet.header", "planet.defenses")
 }
